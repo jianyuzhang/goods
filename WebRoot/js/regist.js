80c770e3d0c54c5c1cf4ef7af6092a79d186c417
馬驍 <wawl89757@126.com>
2015-03-06 15:23:32 +0800

    修改了注册页面的错误提示

---
+++ WebRoot/js/regist.js
@@ -0,0 +1,101 @@
+var app = angular.module("regist", []);
+angularConfig(app);
+
+app.controller('registCtrol', function($scope, $http, $element) {
+	$scope.title = '注册页';
+
+	$scope.errorTips = {
+		'loginname' : '登录名不能为空',
+		'password' : '密码不能为空',
+		'repassword' : '确认密码不能为空',
+		'email' : '邮箱不能为空'
+	};
+
+	$scope.change = function(target) {
+		if (!$scope[target]) {
+			$element.find('input[ng-model="' + target + '"]').popover({
+				content : $scope.errorTips[target],
+				placement : 'top',
+				trigger: 'manual'
+			});
+			$element.find('input[ng-model="' + target + '"]').popover('show');
+		} else {
+			$element.find('input[ng-model="' + target + '"]').popover('destroy');
+		}
+	};
+
+	$scope.submit = function() {
+		if ($scope.registForm.$valid) {
+			$http.post("/goods/operate/user/registUser.do", {
+				loginname : $scope.loginname,
+				loginpass : $scope.password,
+				email : $scope.email,
+			}).success(function(result) {
+				if (!result) {
+					$scope.errorMsg = '注册失败';
+					$("#myModal").modal({
+						"backdrop" : "static",
+						"keyboard" : true,
+						"show" : true
+					});
+				} else {
+					window.location.href = "/goods/index.html";
+				}
+			});
+		}
+
+		// if (!$scope.loginname) {
+		// $scope.errorMsg = '登录名不能为空';
+		// $("#myModal").modal({
+		// "backdrop" : "static",
+		// "keyboard" : true,
+		// "show" : true
+		// });
+		// } else if (!$scope.password) {
+		// $scope.errorMsg = '密码不能为空';
+		// $("#myModal").modal({
+		// "backdrop" : "static",
+		// "keyboard" : true,
+		// "show" : true
+		// });
+		// } else if (!$scope.repassword) {
+		// $scope.errorMsg = '确认密码不能为空';
+		// $("#myModal").modal({
+		// "backdrop" : "static",
+		// "keyboard" : true,
+		// "show" : true
+		// });
+		// } else if (!$scope.email) {
+		// $scope.errorMsg = '邮箱不能为空';
+		// $("#myModal").modal({
+		// "backdrop" : "static",
+		// "keyboard" : true,
+		// "show" : true
+		// });
+		// } else if (!$scope.verifyCode) {
+		// $scope.errorMsg = '验证码不能为空';
+		// $("#myModal").modal({
+		// "backdrop" : "static",
+		// "keyboard" : true,
+		// "show" : true
+		// });
+		// } else {
+		// $http.post("/goods/operate/user/registUser.do", {
+		// loginname : $scope.loginname,
+		// loginpass : $scope.password,
+		// email : $scope.email,
+		// }).success(function(result) {
+		// if (!result) {
+		// $scope.errorMsg = '注册失败';
+		// $("#myModal").modal({
+		// "backdrop" : "static",
+		// "keyboard" : true,
+		// "show" : true
+		// });
+		// } else {
+		// window.location.href = "/goods/index.html";
+		// }
+		// });
+		// }
+	};
+});
