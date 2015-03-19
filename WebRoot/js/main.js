f1340d71b3c737dbd8c23efa7c5e6714332890dd
馬驍 <wawl89757@126.com>
2015-03-19 10:18:14 +0800

    修复了菜单点击无效的问题

--- WebRoot/js/main.js
+++ WebRoot/js/main.js
@@ -4,12 +4,15 @@ angularConfig(app);
 app.controller('menuCtrol',function($scope,$http){
 	$scope.title = '主页';
 	
+	$scope.slideToggle = function($event) {
+		$($event.target).parents('li').find("ul").slideToggle();
+	};
 	
-	$http.post("/goods/operate/menu/showMenus.do",{
-		
-	}).success(function(MenuDTOParents){
+	$http.post("/goods/operate/menu/showMenus.do").success(function(MenuDTOParents){
 		$scope.MenuDTOParents = MenuDTOParents;
-		
+//		$(".dropmenu").click(function(t) {
+//			t.preventDefault();
+//		})
 	});
 	
 });
