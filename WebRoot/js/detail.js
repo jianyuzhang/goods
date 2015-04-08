2ddb5eb9c70abd486650a7d3f299d0891dfb02ee
jianyuzhang <115034638@qq.com>
2015-04-08 16:25:03 +0800

    单品展示

--- WebRoot/js/detail.js
+++ WebRoot/js/detail.js
@@ -1,7 +1,15 @@
 var app = angular.module('detail', []);
 app.controller('detailCtrol',function($scope,$http){
+	$scope.cd=$scope.cd[0]
+		console.log($scope.cd[0])
+	$scope.num=1;
 	
-		console.log($scope.cd)
-	
-	
+	$scope.minus = function (){
+		if($scope.num>1){
+		$scope.num--;
+	}
+	}
+	$scope.plus = function (){
+		$scope.num++;
+	}
 });
