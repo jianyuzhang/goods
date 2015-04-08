1881ae8e82e1164f23a9eab1b8ef945f3f06886a
jianyuzhang <115034638@qq.com>
2015-04-08 17:21:47 +0800

    购物车后台

--- src/com/zjy/service/CartItemService.java
+++ src/com/zjy/service/CartItemService.java
@@ -1,6 +1,26 @@
 package com.zjy.service;
 
+import java.util.List;
+
+import com.zjy.models.CartItem;
+
 
 public interface CartItemService {
+	/*
+	 * 添加购物车
+	 */
+	  public int createCart(CartItem cart);
+	  /*
+	   * 展示对应用户的购物车所有的商品
+	   */
+	  public List<CartItem> showAllcarts(String[] properties, Object[] propertyValues);
 	  
+	  /*
+	   * 修改
+	   */
+	  public int updateCartItem(CartItem cart);
+	  /*
+	   * 删除购物车里商品
+	   */
+	  public void deleteCartItem(String cartItemId);
 }
--- src/com/zjy/service/serviceImpl/CartItemServiceImpl.java
+++ src/com/zjy/service/serviceImpl/CartItemServiceImpl.java
@@ -1,10 +1,13 @@
 package com.zjy.service.serviceImpl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.zjy.daos.CartItemDao;
+import com.zjy.models.CartItem;
 import com.zjy.service.CartItemService;
 
 @Service("cartItemService")
@@ -12,6 +15,45 @@ public class CartItemServiceImpl implements CartItemService {
 	
 	@Resource
 	private CartItemDao cartItemDao;
+   /*
+    * 插入新的商品
+    * @see com.zjy.service.CartItemService#createCart(com.zjy.models.CartItem)
+    */
+	@Override
+	public int createCart(CartItem cart) {
+		int  i = cartItemDao.insert(cart);
+		return i;
+	}
+	/*
+	 * 展示某人的购物车
+	 * @see com.zjy.service.CartItemService#showAllcarts(java.lang.String[], java.lang.Object[])
+	 */
+	@Override
+	public List<CartItem> showAllcarts(String[] properties,
+			Object[] propertyValues) {
+		
+		List<CartItem> carts=cartItemDao.findByStatementPostfix("", properties, propertyValues, null, null);
+		return carts;
+	}
+	/*
+	 * 修改购物车
+	 * @see com.zjy.service.CartItemService#updateCartItem(com.zjy.models.CartItem)
+	 */
+	@Override
+	public int updateCartItem(CartItem cart) {
+		// TODO Auto-generated method stub
+		int i = cartItemDao.update(cart);
+		return i;
+	}
+	/*
+	 * 删除购物车内商品
+	 * @see com.zjy.service.CartItemService#deleteCartItem(java.lang.String)
+	 */
+	@Override
+	public void deleteCartItem(String cartItemId) {
+		// TODO Auto-generated method stub
+		cartItemDao.deleteById(cartItemId);
+	}
 
    
 }
