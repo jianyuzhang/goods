f2a7db8f8b1d6c6169f4b33d1ff22c4b1fad9434
jianyuzhang <115034638@qq.com>
2015-04-14 17:30:17 +0800

    删除功能

--- WebRoot/js/cart.js
+++ WebRoot/js/cart.js
@@ -1,7 +1,62 @@
 var app = angular.module('cart', []);
-app.controller('cartCtrol', function($scope, $http, $element) {
+app.controller('cartCtrol', function($scope, $http, $element, $compile) {
+	$scope.status = 0;
+     console.log($element.find('input.checkOne'))
+		
+	
+	
 	$http.post("/goods/operate/cartItem/showSomeCarts.do",{uid:$scope.uid}).success(function(carts){
 		$scope.carts = carts;
-		console.log($scope.carts)
+		//console.log($scope.carts)
 	});
+	$scope.plus = function(cart){
+	//console.log(event.currentTarget.parentNode.childNodes[3].value)
+		cart.quantity++;
+		$http.post("/goods/operate/cartItem/updateCart.do", {
+			uid : $scope.uid,
+			cid : cart.cid,
+			num : cart.quantity
+		}).success(function() {
+
+		});
+	}
+	$scope.minus = function(cart){
+		//console.log(event.currentTarget.parentNode[3].value)
+		if(cart.quantity>0){
+			cart.quantity--;
+			$http.post("/goods/operate/cartItem/updateCart.do", {
+				uid : $scope.uid,
+				cid : cart.cid,
+				num : cart.quantity
+			}).success(function() {
+
+			});
+			}
+	}
+	$scope.update = function(cart){
+		var validateNum = /^[0-9]*[1-9][0-9]*$/;
+		console.log(validateNum.test(cart.quantity))
+		if (!validateNum.test(cart.quantity) || cart.quantity == '') {
+			cart.quantity = 1;
+		}
+		$http.post("/goods/operate/cartItem/updateCart.do", {
+			uid : $scope.uid,
+			cid : cart.cid,
+			num : cart.quantity
+		}).success(function() {
+
+		});
+	}
+	$scope.$watch('carts', change, true);
+	
+	function change(){
+		var total = 0;
+		for (var i = 0; i < $scope.carts.length; i++) {
+	          var p = $scope.carts[i].currPrice;
+	          var quantity = $scope.carts[i].quantity;
+	         total  += p*quantity;
+	        }
+	       console.log(total);
+	       $scope.total = total;
+	}
 });
